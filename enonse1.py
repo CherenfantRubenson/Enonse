8714e06703e019f8a97426531baeba57a43cb9e2
Rubenson Cherenfant <92955778+CherenfantRubenson@users.noreply.github.com>
2023-11-03 16:16:13 -0400

    Enonse 4

--- enonse1.py
+++ enonse1.py
@@ -1,7 +1,33 @@
-teks ="Mwen Renmen Ayiti"
+#Enonse 1
+# teks ="Mwen Renmen Ayiti"
 
-teks = teks.lower()
+# teks = teks.lower()
 
-print(teks)
+# print(teks)
+
+#Enonse 2
+# teks ="Ayibobo Ayiti"
+
+# alpha = teks.split(" ")
+
+# print(alpha)
+
+#Enonse 3
+
+# teks = "sa yo enpotan anpil"
+
+# teks = teks.title()
+# print(teks)
+
+
+Eleman = "Offis Proteksyon Sitwayen"
+jwenn = Eleman.split()
+
+premyelet= []
+
+for let in jwenn:
+    premyelet.append(let[0])
+
+print(premyelet)
 
 
